a87b152c06e5480d5bcfbcdec72d0fa8fa0d9a78
ADE YOGA WIYADNYANA <85540115+yorisofficial@users.noreply.github.com>
2022-10-22 11:53:34 +0800

    Add files via upload

---
+++ assets/js/javascript.js
@@ -0,0 +1,12 @@
+const body = document.querySelector("body");
+const switchBtn = document.getElementById("switch");
+const root = document.querySelector(":root");
+
+// Change theme
+switchBtn.addEventListener("change", () => {
+  toggleRootClass();
+});
+
+function toggleRootClass() {
+  document.querySelector(":root").classList.toggle("dark");
+}
